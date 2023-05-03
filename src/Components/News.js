05d56d277d913805e26fc183c119e64414911d7b
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-05-03 19:17:50 +0530

    03-05-2023 updated

--- src/Components/News.js
+++ src/Components/News.js
@@ -60,7 +60,7 @@ export class News extends Component {
   render() {
     return (
       <>
-          <h1 className='text-center' style={{ margin: "35px", marginTop: "90px"}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+          <h1 className='text-center' style={{ margin: "35px", marginTop: "90px"}}>NewsAgent - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
           {this.state.loading && <Spinner/>}
           <InfiniteScroll
             dataLength={this.state.articles.length}
